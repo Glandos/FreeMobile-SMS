bfb2fe014dcef46b2decedce3621027284615637
Adrien CLERC <adrien@antipoul.fr>
2015-01-23 21:43:33 +0100

    Initial commit

---
+++ config.sample.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+users = {
+    'Jean-jacques': ('12345678', 'api-key'),
+}
---
+++ sms_send.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import requests
+
+from flask import Flask
+from flask import render_template, jsonify, request
+
+from config import users
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return render_template('send_sms.html', dests=users.keys())
+
+@app.route('/send', methods=['POST'])
+def send():
+    text = request.form['text']
+    dest = request.form['dest']
+    user, key = users.get(dest, (None, None))
+    if user and text:
+        # The doc says that POST is supported, but test says it is not.
+        r = requests.get('https://smsapi.free-mobile.fr/sendmsg',
+                          params={'user': user, 'pass': key, 'msg': text}
+                         )
+        return jsonify(code=r.status_code)
+    return jsonify(code=400)
+
+if __name__ == '__main__':
+    app.run(debug=True)
